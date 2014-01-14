1ab4f527e11093213609baf563f7536bb1ab0d30
jgreggors <jgregg44@gmail.com>
2014-01-13 17:33:24 -0800

    More efficient movement function

--- MansionMashers/MansionMashers/HeaderFiles/Movement.h
+++ MansionMashers/MansionMashers/HeaderFiles/Movement.h
@@ -1,6 +1,6 @@
-#include <Windows.h>
+
 
 typedef enum { UP, LEFT, DOWN, RIGHT } Direction;
 
 
-void JG_move(int key, float *objX, float *objY, float move_dist, int direction);
+void JG_move(int key, struct Sprite *obj, float move_dist, int direction);
