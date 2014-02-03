52e5f6ad71ac846d78e5ab99d298b01a73d14dd1
jgreggors <jgregg44@gmail.com>
2014-02-02 22:42:47 -0800

    Sounds be a workin

--- MansionMashers/MansionMashers/HeaderFiles/Sound.h
+++ MansionMashers/MansionMashers/HeaderFiles/Sound.h
@@ -9,8 +9,8 @@ FMOD_SYSTEM * GetSoundSystem(void);
 void FMODErrCheck(FMOD_RESULT result);
 void FMODInit(void);
 void FMODQuit(void);
-void CreateSound(char *Filename, FMOD_SOUND *sound);
-void Play_Sound(FMOD_SOUND *sound, FMOD_CHANNEL *channel);
+void CreateSound(char *Filename, FMOD_SOUND **sound);
+void PlayAudio(FMOD_SOUND *sound, FMOD_CHANNEL **channel);
 void ReleaseSound(FMOD_SOUND *sound);
 
 #endif
