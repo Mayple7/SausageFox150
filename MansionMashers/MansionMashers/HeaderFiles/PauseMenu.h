934f045d0c020a04eb51a80d968caeee77abc99e
Mayple7 <mayple777@gmail.com>
2014-02-03 12:25:19 -0800

    Update to the icon and have basic pause functionality working in showcase level.

---
+++ MansionMashers/MansionMashers/HeaderFiles/PauseMenu.h
@@ -0,0 +1,11 @@
+#ifndef FOX_PAUSE
+#define FOX_PAUSE
+
+void LoadPause(void);
+void InitializePause(void (*DrawLevel));
+void UpdatePause(void);
+void DrawPause(void);
+void FreePause(void);
+void UnloadPause(void);
+
+#endif
