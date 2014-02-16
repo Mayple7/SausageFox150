0f355ca0837b5ae8dba4f751793c6d80821c7d2a
Mayple7 <mayple777@gmail.com>
2014-02-15 22:37:43 -0800

    Created and added "TemplateLevel" both source and header so it is easy to create another level.

---
+++ MansionMashers/MansionMashers/HeaderFiles/TemplateLevel.h
@@ -0,0 +1,13 @@
+#ifndef FOX_LEVEL1
+#define FOX_LEVEL1
+
+void LoadLevel1(void);
+void InitializeLevel1(void);
+void UpdateLevel1(void);
+void DrawLevel1(void);
+void FreeLevel1(void);
+void UnloadLevel1(void);
+
+void EventLevel1(void);
+
+#endif
