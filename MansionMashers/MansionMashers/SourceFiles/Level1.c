2287204e26e9dabc3439734b6586f0bb4061c1b4
jgreggors <jgregg44@gmail.com>
2014-02-17 13:12:05 -0800

    More Level1

--- MansionMashers/MansionMashers/SourceFiles/Level1.c
+++ MansionMashers/MansionMashers/SourceFiles/Level1.c
@@ -106,6 +106,10 @@ void UpdateLevel1(void)
 {
 	EventLevel1();
 
+	if(CurrentPlayer.PlayerCollider.Position.x - CurrentPlayer.PlayerCollider.width / 2 < -7 * Level1Panel1->Width / 16)
+			CurrentPlayer.Position.x = (-7 * Level1Panel1->Width / 16) + (CurrentPlayer.PlayerCollider.width / 2) + 1;
+
+
 	// This should be the last line in this function
 	UpdatePlayerPosition(&CurrentPlayer);
 }
