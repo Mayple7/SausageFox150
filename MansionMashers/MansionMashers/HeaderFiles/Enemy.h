f2c594a53581d65d7e9effc1c2cfe8e9cf384719
JoeFoxe <joefoxe2012@gmail.com>
2014-03-09 22:21:11 -0700

    yea

--- MansionMashers/MansionMashers/HeaderFiles/Enemy.h
+++ MansionMashers/MansionMashers/HeaderFiles/Enemy.h
@@ -94,7 +94,7 @@ typedef struct Enemy
 }Enemy;
 
 void LoadEnemy(int enemyType);
-Enemy* CreateEnemy(int enemyType, int collisionGroup, int objID, float xPos, float yPos);
+Enemy* CreateEnemy(int enemyType, int collisionGroup, int objID, float xPos, float yPos, int panelId);
 void InitializeEnemyStats(Enemy *CurrentEnemy, int maxHP, float movSpeed, float atkSpeed, float dmgReduction, int dmg, int money, int exp);
 void UpdateEnemy(Enemy *CurrentEnemy);
 void DetectEnemyCollision(Enemy *CurrentEnemy);
