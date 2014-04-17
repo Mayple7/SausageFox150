d7a263e488385f104bf9abdd187907215dd524b9
jgreggors <jgregg44@gmail.com>
2014-04-16 19:19:18 -0700

    BUG OF ALL BUGS I MADE THE WRONG HEAD! AND THEN STRETCHED IT! ughghhhhhhhhh lol

--- MansionMashers/MansionMashers/SourceFiles/Narr2.c
+++ MansionMashers/MansionMashers/SourceFiles/Narr2.c
@@ -215,7 +215,7 @@ void InitializeNarr2(void)
 		PlayerHead = (Sprite*)CreateSprite("TextureFiles/GinkoHead.png", HEAD_SCALE * 129.75f, HEAD_SCALE * 101.25f, 30, 1, 1, -310, 310);
 		break;
 	case Holly:
-		PlayerHead = (Sprite*)CreateSprite("TextureFiles/MaypleHead.png", HEAD_SCALE * 140.25f, HEAD_SCALE * 108.75f, 30, 1, 1, -310, 310);
+		PlayerHead = (Sprite*)CreateSprite("TextureFiles/HollyHead.png", HEAD_SCALE * 140.25f, HEAD_SCALE * 108.75f, 30, 1, 1, -310, 310);
 		break;
 	}
 
